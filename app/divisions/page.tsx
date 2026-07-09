3f15c6ecf568757b0169162370a2f88cb824d32e
wiil404 <pengaliciousrx@gmail.com>
2026-07-09 20:03:19 +0100

    Update page.tsx

---
+++ app/divisions/page.tsx
@@ -0,0 +1,42 @@
+export default function DivisionsPage(){
+
+return (
+
+<main className="
+max-w-7xl
+mx-auto
+px-6
+py-20
+">
+
+<div className="
+bg-white
+shadow-xl
+p-12
+">
+
+<h1 className="
+text-5xl
+font-bold
+text-[#003B6F]
+">
+
+Our Mission
+
+</h1>
+
+
+<p className="mt-6 text-lg text-gray-700">
+
+Explore DHS divisions and operational areas.
+
+</p>
+
+
+</div>
+
+</main>
+
+);
+
+}
