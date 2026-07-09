8055a230aaf0176fb020a2694d7e3c3fb89f4d3d
wiil404 <pengaliciousrx@gmail.com>
2026-07-09 20:03:43 +0100

    Update page.tsx

---
+++ app/documents/page.tsx
@@ -0,0 +1,42 @@
+export default function DocumentsPage(){
+
+return (
+
+<main className="
+max-w-7xl
+mx-auto
+px-6
+py-20
+">
+
+<div className="
+bg-white
+shadow-xl
+p-12
+">
+
+<h1 className="
+text-5xl
+font-bold
+text-[#003B6F]
+">
+
+Resources
+
+</h1>
+
+
+<p className="mt-6 text-lg text-gray-700">
+
+Public documents and resources.
+
+</p>
+
+
+</div>
+
+</main>
+
+);
+
+}
