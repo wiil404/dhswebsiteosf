430ef5e2c4edd321b582ca39cfe988e8055df3ca
wiil404 <pengaliciousrx@gmail.com>
2026-07-09 20:03:52 +0100

    Update page.tsx

---
+++ app/recruitment/page.tsx
@@ -0,0 +1,42 @@
+export default function RecruitmentPage(){
+
+return (
+
+<main className="
+max-w-7xl
+mx-auto
+px-6
+py-20
+">
+
+<div className="
+bg-white
+shadow-xl
+p-12
+">
+
+<h1 className="
+text-5xl
+font-bold
+text-[#003B6F]
+">
+
+Careers
+
+</h1>
+
+
+<p className="mt-6 text-lg text-gray-700">
+
+Join the Department of Homeland Security.
+
+</p>
+
+
+</div>
+
+</main>
+
+);
+
+}
