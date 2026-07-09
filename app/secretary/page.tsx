a5818f508dfd0614c06dfd3426011ea742e0ccba
wiil404 <pengaliciousrx@gmail.com>
2026-07-09 20:04:03 +0100

    Update page.tsx

---
+++ app/secretary/page.tsx
@@ -0,0 +1,42 @@
+export default function SecretaryPage(){
+
+return (
+
+<main className="
+max-w-7xl
+mx-auto
+px-6
+py-20
+">
+
+<div className="
+bg-white
+shadow-xl
+p-12
+">
+
+<h1 className="
+text-5xl
+font-bold
+text-[#003B6F]
+">
+
+Secretary of Homeland Security
+
+</h1>
+
+
+<p className="mt-6 text-lg text-gray-700">
+
+Official office of the Secretary.
+
+</p>
+
+
+</div>
+
+</main>
+
+);
+
+}
