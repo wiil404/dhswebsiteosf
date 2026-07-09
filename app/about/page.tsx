3471c7fb2d9aa25ce10bdf12615be4fb6e45ef16
wiil404 <pengaliciousrx@gmail.com>
2026-07-09 20:03:03 +0100

    Update page.tsx

---
+++ app/about/page.tsx
@@ -0,0 +1,41 @@
+export default function AboutPage(){
+
+    return (
+
+        <main className="
+            max-w-7xl
+            mx-auto
+            px-6
+            py-20
+        ">
+
+            <div className="
+                bg-white
+                shadow-xl
+                p-12
+            ">
+
+                <h1 className="
+                    text-5xl
+                    font-bold
+                    text-[#003B6F]
+                ">
+                    About DHS
+                </h1>
+
+
+                <p className="
+                    mt-6
+                    text-lg
+                    text-gray-700
+                ">
+                    Learn more about the Department of Homeland Security.
+                </p>
+
+            </div>
+
+        </main>
+
+    );
+
+}
