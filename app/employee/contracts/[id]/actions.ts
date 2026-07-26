dbaa337c6a4d890858fe7cf020ce7ecbb1c0e737
wiil404 <pengaliciousrx@gmail.com>
2026-07-26 13:53:43 +0100

    Update actions.ts

--- app/employee/contracts/[id]/actions.ts
+++ app/employee/contracts/[id]/actions.ts
@@ -18,8 +18,58 @@ if(!session){
 }
 
 
-
 const employee = session.employees;
+
+
+
+const {data:contract,error:contractError}=await supabaseAdmin
+
+.from("contracts")
+
+.select(
+`
+employee_signed,
+executive_signed
+`
+)
+
+.eq(
+"id",
+contractId
+)
+
+.single();
+
+
+
+if(contractError || !contract){
+
+throw new Error("Contract not found");
+
+}
+
+
+
+
+let status =
+"Pending Employee Signature";
+
+
+if(contract.executive_signed){
+
+status="Completed";
+
+}
+
+else{
+
+status="Awaiting Executive Signature";
+
+}
+
+
+
+
 
 
 
@@ -38,7 +88,10 @@ employee_signature_id:
 employee.roblox_user_id,
 
 employee_signature_date:
-new Date()
+new Date(),
+
+
+status
 
 })
 
@@ -46,6 +99,8 @@ new Date()
 "id",
 contractId
 );
+
+
 
 
 
@@ -72,6 +127,7 @@ details:
 `${employee.roblox_username} signed contract ${contractId}`
 
 });
+
 
 
 
