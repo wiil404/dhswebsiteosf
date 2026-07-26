9a03eaeea2de110a166d8f1049f676e766f5f914
wiil404 <pengaliciousrx@gmail.com>
2026-07-26 14:18:05 +0100

    Create actions.ts

---
+++ app/staff/contracts/[id]/actions.ts
@@ -0,0 +1,133 @@
+"use server";
+
+import { supabaseAdmin } from "@/app/lib/supabase-admin";
+import { redirect } from "next/navigation";
+
+
+export async function signExecutiveContract(
+    contractId:string
+){
+
+
+const executiveName = "WiIl404";
+const executiveRobloxId = "333195903";
+
+
+
+const {error}=await supabaseAdmin
+
+.from("contracts")
+
+.update({
+
+executive_signed:true,
+
+executive_signature_name:
+executiveName,
+
+executive_signature_id:
+executiveRobloxId,
+
+executive_signature_date:
+new Date()
+
+
+})
+
+.eq(
+"id",
+contractId
+);
+
+
+
+
+if(error){
+
+throw new Error(error.message);
+
+}
+
+
+
+
+
+// Update status
+
+const {data:contract}=await supabaseAdmin
+
+.from("contracts")
+
+.select(
+`
+employee_signed,
+executive_signed
+`
+)
+
+.eq(
+"id",
+contractId
+)
+
+.single();
+
+
+
+
+
+if(
+contract?.employee_signed &&
+contract?.executive_signed
+){
+
+await supabaseAdmin
+
+.from("contracts")
+
+.update({
+
+status:"Completed"
+
+})
+
+.eq(
+"id",
+contractId
+);
+
+
+
+}
+
+else if(
+contract?.employee_signed
+){
+
+await supabaseAdmin
+
+.from("contracts")
+
+.update({
+
+status:"Awaiting Executive Signature"
+
+})
+
+.eq(
+"id",
+contractId
+);
+
+
+}
+
+
+
+
+redirect(
+`/staff/contracts/${contractId}`
+);
+
+
+}
