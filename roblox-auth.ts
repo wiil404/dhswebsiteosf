f6e5d36513f6004dd6cacc9102ddd7ed7518ac5a
wiil404 <pengaliciousrx@gmail.com>
2026-07-26 01:36:18 +0100

    Create roblox-auth.ts

---
+++ roblox-auth.ts
@@ -0,0 +1,30 @@
+export function getRobloxLoginURL(){
+
+    const clientId = process.env.ROBLOX_CLIENT_ID;
+
+    const redirectUri = process.env.ROBLOX_REDIRECT_URI;
+
+
+    if(!clientId || !redirectUri){
+
+        return "#";
+
+    }
+
+
+    const params = new URLSearchParams({
+
+        client_id: clientId,
+
+        response_type: "code",
+
+        redirect_uri: redirectUri,
+
+        scope: "openid profile",
+
+    });
+
+
+    return `https://apis.roblox.com/oauth/v1/authorize?${params.toString()}`;
+
+}
