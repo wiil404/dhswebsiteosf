fa2fd6c5a3d9bca076d0c3ec2bf2e0090051018f
wiil404 <pengaliciousrx@gmail.com>
2026-07-26 01:41:06 +0100

    Update roblox-auth.ts

--- app/lib/roblox-auth.ts
+++ app/lib/roblox-auth.ts
@@ -1,28 +1,30 @@
 export function getRobloxLoginURL(){
 
+    const clientId = process.env.ROBLOX_CLIENT_ID;
+
+    const redirectUri = process.env.ROBLOX_REDIRECT_URI;
+
+
+    if(!clientId || !redirectUri){
+
+        return "#";
+
+    }
+
+
     const params = new URLSearchParams({
 
-        client_id:
-        process.env.ROBLOX_CLIENT_ID!,
+        client_id: clientId,
 
-        response_type:"code",
+        response_type: "code",
 
-        redirect_uri:
-        process.env.ROBLOX_REDIRECT_URI!,
+        redirect_uri: redirectUri,
 
-        scope:"openid profile"
+        scope: "profile",
 
     });
 
 
-    return (
-
-        "https://apis.roblox.com/oauth/v1/authorize?"
-
-        +
-
-        params.toString()
-
-    );
+    return `https://apis.roblox.com/oauth/v1/authorize?${params.toString()}`;
 
 }
