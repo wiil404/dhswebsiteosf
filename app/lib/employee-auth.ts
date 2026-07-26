0f54194b0392e6879519ef7790fd9035fae1f3e8
wiil404 <pengaliciousrx@gmail.com>
2026-07-26 01:48:10 +0100

    Add files via upload

---
+++ app/lib/employee-auth.ts
@@ -0,0 +1,79 @@
+import { cookies } from "next/headers";
+import { supabaseAdmin } from "./supabase-admin";
+
+
+export async function getEmployeeSession(){
+
+
+    const cookieStore = await cookies();
+
+
+    const sessionId =
+        cookieStore.get("employee_session")?.value;
+
+
+
+    if(!sessionId){
+
+        return null;
+
+    }
+
+
+
+
+    const {
+
+        data:session
+
+    } = await supabaseAdmin
+
+        .from("employee_sessions")
+
+        .select(`
+
+            *,
+
+            employees(
+
+                id,
+                roblox_username,
+                roblox_user_id,
+                employee_number,
+                status,
+
+                divisions(
+                    name
+                ),
+
+                positions(
+                    title
+                )
+
+            )
+
+        `)
+
+        .eq(
+            "id",
+            sessionId
+        )
+
+        .single();
+
+
+
+
+
+    if(!session){
+
+        return null;
+
+    }
+
+
+
+
+    return session;
+
+}
